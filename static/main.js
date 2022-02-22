f1454f79f5df9a9af73a0aea5dc65cf16447a580
Nayeong An <dsd932@naver.com>
2022-02-22 21:37:22 +0900

    JavaScript에서 css 수정

--- static/main.js
+++ static/main.js
@@ -8,6 +8,14 @@ function checkChat() {
     else if(chat == "뭐해?") cat.innerText = "meow~";
     else if(chat == "사랑해") cat.innerText = "냐아아아아옹~~~";
     else if(chat == "바보" || chat == "멍청이") alert("욕설은 안돼요!");
+    else if(chat == "불꺼줘") {
+        document.body.style.backgroundColor = "#5D5D5D";
+        cat.innerText = "얏홍"
+    }
+    else if(chat == "불켜줘") {
+        document.body.style.backgroundColor = "#ffffff";
+        cat.innerText = "애옹"
+    } 
     console.log(chat);
 }
 
