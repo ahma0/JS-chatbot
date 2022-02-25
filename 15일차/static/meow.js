b9ab9a2604077e1b3afe84d4d4da10ca87fd8df0
Nayeong An <dsd932@naver.com>
2022-02-25 15:56:15 +0900

    말 배우기 기능

---
+++ 15일차/static/meow.js
@@ -0,0 +1,22 @@
+let json = [
+    {
+        "question" : "안녕",
+        "answer" : "안냥"
+    },
+    {
+        "question" : "이름",
+        "answer" : "애옹~"
+    },
+    {
+        "question" : "사랑해",
+        "answer" : "냐아아아옹"
+    },
+    {
+        "question" : "알라뷰",
+        "answer" : "얏홍"
+    },
+    {
+        "question" : "뭐해?",
+        "answer" : "사냥"
+    }
+]
